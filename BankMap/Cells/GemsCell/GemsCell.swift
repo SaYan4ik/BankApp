3edf7c941fa73540603bf01c924c8b960b1491e4
SaYan4ik <66200306+SaYan4ik@users.noreply.github.com>
2023-01-19 20:34:37 +0300

    updateGemsController
    add tableView
    add gem cell
    take info about gems

--- BankMap/Cells/GemsCell/GemsCell.swift
+++ BankMap/Cells/GemsCell/GemsCell.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class GemsCell: UITableViewCell {
-
+    @IBOutlet weak var attestatLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var edgeLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var colorLabel: UILabel!
+    @IBOutlet weak var costLabel: UILabel!
+    @IBOutlet weak var bankNameLabel: UILabel!
+    
+    @IBOutlet weak var container: UIStackView!
+    
     static var id = String(describing: GemsCell.self)
     
     override func awakeFromNib() {
         super.awakeFromNib()
     }
+    
+    func set(gem: GemModel) {
+        self.attestatLabel.text = gem.attestat
+        self.nameLabel.text = gem.name
+        self.edgeLabel.text = gem.grani
+        self.weightLabel.text = gem.weight
+        self.colorLabel.text = gem.color
+        self.costLabel.text = gem.cost
+        self.bankNameLabel.text = "\(gem.cityType)\(gem.city) \(gem.filialName)"
+        setupData()
+    }
+    
+    private func setupData() {
+        container.layer.cornerRadius = 12
+    }
+    
 }
--- BankMap/GemsController/GemsController.swift
+++ BankMap/GemsController/GemsController.swift
@@ -9,10 +9,44 @@ import UIKit
 
 class GemsController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var spiner: UIActivityIndicatorView!
+    
+    private var gemsData = [GemModel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getGemData()
+        registerCell()
+        tableView.dataSource = self
 
     }
+    
+    private func getGemData() {
+        spiner.startAnimating()
+        GetBankInfo().getGemsInfo(city: "") { [weak self] gems in
+            guard let self else { return }
+            self.gemsData = gems
+            self.tableView.reloadData()
+            self.spiner.stopAnimating()
+        }
+    }
+    
+    private func registerCell() {
+        let nib = UINib(nibName: GemsCell.id, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: GemsCell.id)
+    }
 
+}
+
+extension GemsController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gemsData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: GemsCell.id, for: indexPath)
+        guard let gemCell = cell as? GemsCell else { return cell }
+        gemCell.set(gem: gemsData[indexPath.row])
+        return gemCell
+    }
 }
